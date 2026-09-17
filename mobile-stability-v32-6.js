36413fceb5a465aa78465fa530d93723b8d5edd5
evertonr415-beep <evertonr415@gmail.com>
2026-09-17 12:07:56 -0300

    feat: load leadership elector preview on mobile

--- mobile-stability-v32-6.js
+++ mobile-stability-v32-6.js
@@ -17,6 +17,7 @@
   function ensureCandidateSupport(){
     loadOnce(location.origin+'/candidate-picker-scroll-guard-v33.js?v=33.2','vf-candidate-scroll-guard-v33');
     loadOnce(location.origin+'/mobile-politician-photos-v27-7.js?v=27.8','vf-politician-photos-v27-8');
+    loadOnce(location.origin+'/leadership-electors-v34.js?v=34','vf-leadership-electors-v34');
   }
 
   function isMobile(){
